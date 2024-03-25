d00ccbe34cdf2e5446badfd83e6368b01e97d52f
smoonmare <smoonmare@gmail.com>
2024-03-25 08:24:42 -0700

    Refine inventory card UI and enhance inventory listing
    - Enhance inventory-card.component.html to display machine details using Material Design Card components, including machine images, names, years, prices, and statuses, providing a more informative and visually appealing UI.
    - Expand inventory-card.component.scss with specific styling for the machine cards, such as fixed image heights, adjusted typography sizes, and margin optimizations for a cleaner and more consistent layout across the inventory display.
    - Update inventory-card.component.ts to include MatCardModule in its imports, ensuring the Material Design Card components are properly recognized and utilized within the standalone component, aligning with Angular's modern development practices.
    - Modify inventory-page.component.html to correct the syntax for the ngFor directive, ensuring unique tracking of each inventory item by their id, which optimizes performance and prevents potential rendering issues.
    
    These improvements significantly enhance the user interface of the inventory system, providing users with a more engaging and clear overview of available machines. The use of Material Design components not only elevates the aesthetic appeal but also ensures consistency and functionality across different parts of the application.

--- angular-client/src/app/features/inventory-card/inventory-card.component.ts
+++ angular-client/src/app/features/inventory-card/inventory-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Machine } from '../../shared/models/machine.model';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-inventory-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './inventory-card.component.html',
   styleUrl: './inventory-card.component.scss'
 })
