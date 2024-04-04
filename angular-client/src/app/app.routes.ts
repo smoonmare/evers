f60dcfc895ee325c966a20b9f4dca9dce2d35e6f
smoonmare <smoonmare@gmail.com>
2024-04-04 08:47:06 -0700

    Add Services and Contact Form components with routing
    - Update app.routes.ts to include routes for the newly created ServicesPageComponent and ContactFormComponent. This addition enhances the application's navigation, allowing users to access a dedicated services page and a contact form.
    - Introduce ServicesPageComponent with HTML and SCSS for displaying information about the services offered by the application. This component aims to provide users with details on what services are available, such as delivery and haul services, contributing to a more informative user experience.
    - Create ContactFormComponent with HTML and SCSS, implementing a simple contact form for user inquiries. This form includes fields for the user's name, email, and message, along with a submission button, facilitating user interaction and communication with the application's administrators or support team.
    - Both components are marked as standalone, adhering to Angular's modern development practices and promoting modular architecture. The lack of external dependencies in the imports array underscores the self-contained nature of these components.
    - The inclusion of basic styling for both components ensures that the new pages are not only functional but also visually integrated with the rest of the application, maintaining a cohesive user interface.
    
    These enhancements significantly expand the application's functionality, providing users with additional resources and means of engagement, thereby improving the overall user experience.

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Router, Routes } from '@angular/router';
 import { InventoryPageComponent } from './core/features/inventory-page/inventory-page.component';
 import { DetailedViewComponent } from './core/features/detailed-view/detailed-view.component';
 import { HomePageComponent } from './core/home-page/home-page.component';
+import { ServicesPageComponent } from './core/services-page/services-page.component';
+import { ContactFormComponent } from './shared/contact-form/contact-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'inventory', component: InventoryPageComponent },
   { path: 'inventory/:id', component: DetailedViewComponent },
+  { path: 'services', component: ServicesPageComponent },
+  { path: 'contact', component: ContactFormComponent }
 ];
