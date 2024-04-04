a6b6edc0d4435bce07e2deba03c776b19b9bb3ce
smoonmare <smoonmare@gmail.com>
2024-04-04 09:38:13 -0700

    Integrate header component and update app structure with styling
    - Modify app.component.html to include the HeaderComponent and a footer within a new '.app-container' div, enhancing the overall layout and providing a consistent navigation interface across the application.
    - Implement app.component.scss with styles defining the structure and appearance of the main app container, header, router outlet, and footer. These styles ensure a cohesive look and feel, contributing to a better user experience.
    - Update app.component.ts to import HeaderComponent and include it in the standalone imports array, establishing HeaderComponent as an essential part of the application's core structure.
    - Create HeaderComponent with HTML, SCSS, TypeScript files, and accompanying test spec. This component features a responsive toolbar with navigation links and a search functionality, elevating the user interface by making navigation intuitive and accessible.
    - The HeaderComponent utilizes Angular Material components (MatToolbarModule, MatButtonModule, MatIconModule) for a modern and stylish appearance, alongside a search toggle mechanism to interactively show and hide a search input field.
    - Introduce styles for the HeaderComponent in header.component.scss, focusing on layout, typography, and interactive elements like navigation links and search functionality. These styles are carefully crafted to align with the application's design language.
    - Add necessary unit tests for HeaderComponent to ensure its functionality is reliable and works as intended.
    
    This update significantly improves the application's navigational structure, making it more user-friendly and visually appealing. The HeaderComponent serves as a central element in guiding users through the application, while the addition of a footer adds a touch of personalization and completes the layout.

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ angular-client/src/app/shared/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  @ViewChild('searchbar') searchbar!: ElementRef;
+  searchText = '';
+  toggleSearch: boolean = false;
+
+  openSearch(): void {
+    this.toggleSearch = true;
+    this.searchbar.nativeElement.focus();
+  }
+
+  closeSearch() {
+    this.searchText = '';
+    this.toggleSearch = false;
+  }
+}
