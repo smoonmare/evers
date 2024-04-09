4e10ad71ec1770ea640b33db6adeb2a5d0188988
smoonmare <smoonmare@gmail.com>
2024-04-09 16:04:46 -0700

    refactor(machine-model): Make Machine model properties nullable
    - Updated the Machine model in the EversServer.Models namespace to mark most properties as nullable using the C# nullable reference type annotation (?).
    - This change allows for the creation and manipulation of Machine instances where certain information may not be available or applicable, enhancing flexibility in data handling.
    - Particularly impacts the properties 'Id', 'Name', 'Description', 'Category', 'Location', 'Images', 'Thumbnail', and 'Status', which can now accept null values, aligning with potential MongoDB document structures that may not include all fields.
    - Maintains 'Year' and 'Price' as non-nullable, assuming these are required fields for every Machine instance, reflecting business logic decisions about essential data for machines.
    
    This refactor improves the robustness of the application's data model by accommodating incomplete data scenarios, facilitating easier integration with MongoDB's flexible schema.

--- EversServer/Models/Machine.cs
+++ EversServer/Models/Machine.cs
@@ -7,33 +7,33 @@ namespace EversServer.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
         [BsonElement("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [BsonElement("year")]
         public int Year { get; set; }
 
         [BsonElement("description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [BsonElement("category")]
-        public string Category { get; set; }
+        public string? Category { get; set; }
 
         [BsonElement("price")]
         public double Price { get; set; }
 
         [BsonElement("location")]
-        public string Location { get; set; }
+        public string? Location { get; set; }
 
         [BsonElement("images")]
-        public string[] Images { get; set; }
+        public string[]? Images { get; set; }
 
         [BsonElement("thumbnail")]
-        public string Thumbnail { get; set; }
+        public string? Thumbnail { get; set; }
 
         [BsonElement("status")]
-        public string Status { get; set; }
+        public string? Status { get; set; }
     }
 }
