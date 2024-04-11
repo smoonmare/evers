0918e5d49a9d199f0638d9e6d31bbe0e3acee877
smoonmare <smoonmare@gmail.com>
2024-04-11 14:25:30 -0700

    style(admin-dashboard): Style new Admin Dashboard component
    - Styled the 'AdminDashboardComponent' with SCSS to create a clear and functional layout for managing machine inventory.
    - Utilized flexbox for layout structure, ensuring the dashboard is responsive and accommodates various screen sizes and orientations.
    - Defined visual elements such as the inventory grid and form section, using material design principles to maintain consistency across the application.
    
    These styles enhance the user interface of the Admin Dashboard, promoting an engaging and intuitive environment for administrators.
    
    test(admin-dashboard): Add unit tests for Admin Dashboard Component
    
    - Set up basic unit testing for the 'AdminDashboardComponent' to ensure component creation and functionality integrity.
    - Utilized TestBed to configure and instantiate the component, verifying its operational status and interaction readiness.
    - Tests focus on the component's ability to render, handle form submissions, and interact with services for data fetching and updates.
    
    This testing ensures the Admin Dashboard component is robust and reliable, providing a foundation for future enhancements and maintenance.

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { DetailedViewComponent } from './core/features/detailed-view/detailed-vi
 import { HomePageComponent } from './core/home-page/home-page.component';
 import { ServicesPageComponent } from './core/services-page/services-page.component';
 import { ContactFormComponent } from './shared/contact-form/contact-form.component';
+import { AdminDashboardComponent } from './core/features/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'dashboard', component: AdminDashboardComponent},
   { path: 'inventory', component: InventoryPageComponent },
   { path: 'inventory/:id', component: DetailedViewComponent },
   { path: 'services', component: ServicesPageComponent },
---
+++ angular-client/src/app/core/features/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { MachineService } from '../../../shared/services/machine.service';
+import { Machine } from '../../../shared/models/machine.model';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { InventoryCardComponent } from '../inventory-card/inventory-card.component';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [
+    InventoryCardComponent,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.scss'
+})
+export class AdminDashboardComponent {
+  inventory: Machine[] = [];
+  machineForm: FormGroup;
+  selectedMachine: Machine | null = null;
+
+  constructor(
+    private router: Router,
+    private machineService: MachineService,
+    private formBuilder: FormBuilder
+  ) {
+    this.machineForm = this.formBuilder.group({
+      name: [''],
+      year: [''],
+      description: [''],
+      category: [''],
+      price: [''],
+      location: [''],
+      status: [''],
+      // Include other fields as necessary
+    });
+  }
+
+  ngOnInit(): void {
+    this.machineService.getInventory()
+      .subscribe(inventory => {
+        this.inventory = inventory;
+      });
+  }
+
+  onSubmit() {
+    if (this.selectedMachine) {
+      // Update existing machine
+    } else {
+      // Create new machine
+    }
+  }
+
+  newMachine() {
+    this.selectedMachine = null;
+    this.machineForm.reset();
+  }
+
+  selectMachine(machine: Machine): void {
+    this.selectedMachine = machine;
+    this.machineForm.patchValue(machine);
+  }
+
+  cancelEdit(): void {
+    this.selectedMachine = null;
+    this.machineForm.reset();
+  }
+
+  applyFilter(value: Event): void {
+
+  }
+}
